75a5413ea08fff56e329c53b09b740bc9b7fc51c
yohei-karikawa <yohei-karikawa@cybozu.co.jp>
2013-09-17 15:13:02 +0900

    a

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var io = require('socket.io').listen(8762);
+var sqlite = require('sqlite3').verbose();
+var db = new sqlite.Database('karishoot.db');
+
+io.sockets.on('connection', function(socket){
+
+    // 新規ユーザー登録
+    socket.on('signup', function(data) {
+        var inSql = 'INSERT INTO user VALUES("' +
+            data.id +'","'+
+            data.name+'",'+ // 名前
+            1 +','+ // レベル
+            3000 +','+ // HP
+            10 +','+ // 攻撃力
+            10 +','+ // 守備力
+            1 +','+ // 素早さ
+            0 +','+ // 経験値
+            0 + // 射幸心ポイント
+        ')';
+        db.run(inSql);
+    });
+});
